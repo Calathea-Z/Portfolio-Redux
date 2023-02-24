7e2b9e00a45bdf4c5e4afbb11926a66c8852d2e2
Calathea-Z <zsykes21@gmail.com>
2023-02-24 13:28:07 -0500

    Formatting contact form.

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,4 +1,4 @@
-import { EnvelopeIcon } from '@heroicons/react/24/solid'
+import { EnvelopeIcon, MapPinIcon } from '@heroicons/react/24/solid'
 
 type Props = {}
 const Contact = (props: Props) => {
@@ -9,7 +9,7 @@ const Contact = (props: Props) => {
       </h3>
 
       <div className='flex flex-col space-y-10'>
-        <h4 className='text-4xl font-semibold text-center decoration-yellow-300/50 underline'>
+        <h4 className='text-4xl font-semibold text-center decoration-yellow-300/70 underline'>
             Get in touch with me.
         </h4>
 
@@ -20,17 +20,28 @@ const Contact = (props: Props) => {
                     zsykes21@gmail.com
                 </p>
             </div>
+            <div className='flex items-center space-x-5 justify-center'>
+                <MapPinIcon className='text-yellow-400 h-7 w-7 animate-pulse self-start' />
+                <div className='flex flex-col space-y-2 items-center'>
+                <p className='text-2xl'>
+                    Asheville, North Carolina
+                </p>
+                <p className='text-xs text-yellow-300/90'>
+                  Open to Relocating / Remote Work
+                </p>
+                </div>
+            </div>
         </div>
 
         <form className='flex flex-col space-y-2 w-fit mx-auto'>
             <div className='flex space-x-2'>
-                <input className='contactInput' type='text' />
-                <input className='contactInput' type='text' />
+                <input className='contactInput' type='text' placeholder='Name' />
+                <input className='contactInput' type='email' placeholder='Email' />
             </div>
 
-            <input className='contactInput' type='text' />
+            <input className='contactInput' type='text' placeholder='Subject' />
 
-            <textarea className='contactInput' />
+            <textarea className='contactInput' placeholder='Message' />
 
             <button className='bg-yellow-400/70 py-5 px-10 rounded-md text-black font-bold'>
                 Submit
